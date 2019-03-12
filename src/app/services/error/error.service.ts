9d76e57d5d5da21d19d6be4d59bbc67404c087a9
Julian Garzon <juliangarzon.j@gmail.com>
2019-03-12 12:39:12 -0500

    comments on global error handler

--- src/app/services/error/error.service.ts
+++ src/app/services/error/error.service.ts
@@ -2,23 +2,31 @@ import { Injectable, ErrorHandler } from '@angular/core';
 import { NotificationService } from '../notification/notification.service'
 import { HttpErrorResponse } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
+
+// ............. SERVICE DESCRIPTION ...................
+//  This is the global error handler, implements the ErrorHandler inferface
+// that handles the errors of the aplications with the handleError function
+// .....................................................
+
 export class ErrorService implements ErrorHandler{
   constructor(private notificationServ:NotificationService) { }
 
   handleError(error) {
+    // implemented function that recives the apps errors and handles it. 
     (error instanceof HttpErrorResponse) ? this.http(error) : this.internal(error);
   }
 
   private http(error:any){
+      // handles the errors that occurred in the http communication
       this.notificationServ.alert('Opps an communication error has occurred','',error.message)
       throw error
   }
 
   private internal(error:Error){
+      // handles the errors that occurred in the internal functionality of the plugins or libraries
       this.notificationServ.alert('Opps an internal error has occurred','',error.message)
       throw error
   }
