c8c59eafcc52a3153236fe017cc73fef1344a234
Julian Garzon <juliangarzon.j@gmail.com>
2019-03-12 18:28:35 -0500

    graphic desing of the forest page, load of the fonts and backgrounds and global declarations of custom font families

--- src/app/pages/forest/forest.page.ts
+++ src/app/pages/forest/forest.page.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forest.page.scss'],
 })
 export class ForestPage implements OnInit {
-
+  private data:any ={
+    title:'Forest',
+    body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit '
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  exploreAction(){
+    alert('si')
   }
 
 }
