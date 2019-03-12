43b30f64158b2ea9990029ee06705ea532af1da0
Julian Garzon <juliangarzon.j@gmail.com>
2019-03-12 18:33:52 -0500

    graphic desing of ocean page

--- src/app/pages/ocean/ocean.page.ts
+++ src/app/pages/ocean/ocean.page.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ocean.page.scss'],
 })
 export class OceanPage implements OnInit {
-
+  private data:any ={
+    title:'Ocean',
+    body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit '
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  exploreAction(){
+    alert('si')
   }
 
 }
